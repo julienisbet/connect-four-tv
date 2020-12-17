4a7cefe8c632c5c963ba4f18aabcae0c80ec1a45
gootieno <geoff.otieno7@gmail.com>
2020-12-17 14:55:03 -0800

    no need for me creating auth.py

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,25 +6,10 @@ import NavBar from './components/NavBar'
 import ProtectedRoute from './components/auth/ProtectedRoute'
 import UsersList from './components/UsersList'
 import User from './components/User'
-import { authenticate } from './services/auth'
 
 function App() {
 	const [authenticated, setAuthenticated] = useState(false)
 	const [loaded, setLoaded] = useState(false)
-
-	useEffect(() => {
-	  (async() => {
-	    const user = await authenticate();
-	    if (!user.errors) {
-	      setAuthenticated(true);
-	    }
-	    setLoaded(true);
-	  })();
-	}, []);
-
-	if (!loaded) {
-	  return null;
-	}
 
 	return (
 		<BrowserRouter>
